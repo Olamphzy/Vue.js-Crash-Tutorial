a7ff483587989f2e069560dbd0062b141a08c2ae
Olamphzy <61218900+Olamphzy@users.noreply.github.com>
2023-12-20 14:58:59 +0100

    Fundamentals of VueJs (v3)

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <footer>
+        <p>Copyright Olamphzy &copy; 2023</p>
+        <router-Link to="/about">About</router-Link>
+    </footer>
+</template>
+
+<style scoped>
+    a{
+        color: #333;
+    }
+    footer{
+        margin-top: 30px;
+        text-align: center;
+    }
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <h1>Version 1.0.0</h1>
+    <router-Link to="/">Go Back</router-Link>
+</template>
